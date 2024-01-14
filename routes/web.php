c6a0aa07190a4d2ce4a7d35c9b7caf81a78be213
jswebschiede <schoeneburg.j@googlemail.com>
2024-01-14 16:20:20 +0100

    refactor routes to be protected

--- routes/web.php
+++ routes/web.php
@@ -28,19 +28,21 @@ Route::get('/', function () {
 Route::get('/user/login', function () {
     return view('user.login');
 })->name('login');
-Route::get('/user/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
 Route::get('/user/create', [UserController::class, 'index'])->name('register');
 Route::post('user.create', [UserController::class, 'create'])->name('user.create');
 Route::post('user.logout', [UserController::class, 'logout'])->name('user.logout');
 Route::post('user.login', [UserController::class, 'login'])->name('user.login');
 
-/* Post Routes */
-Route::get('/post/create', [PostController::class, 'index'])->name('post.create');
-Route::get('/post/{post}/edit', function () {
-    return view('post.edit', [
-        'post' => Post::find(request()->post)
-    ]);
-})->name('post.edit');
-Route::post('post.store', [PostController::class, 'store'])->name('post.store');
-Route::patch('/post/{post}', [PostController::class, 'update'])->name('post.update');
-Route::delete('/post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
+/* Protected Routes */
+Route::middleware(['auth'])->group(function () {
+    Route::get('/user/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
+    Route::get('/post/create', [PostController::class, 'index'])->name('post.create');
+    Route::get('/post/{post}/edit', function () {
+        return view('post.edit', [
+            'post' => Post::find(request()->post)
+        ]);
+    })->name('post.edit');
+    Route::post('post.store', [PostController::class, 'store'])->name('post.store');
+    Route::patch('/post/{post}', [PostController::class, 'update'])->name('post.update');
+    Route::delete('/post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
+});
